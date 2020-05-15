c81e723bc3d14e0f2774bd9333173c3b1a1ae7e9
Krzysztof Sakwerda <ksakwerda@gmail.com>
2020-05-15 11:41:32 +0200

    feat: basic rules set

---
+++ src/index.js
@@ -0,0 +1,146 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    "shared-node-browser": true,
+    "cypress/globals": true,
+    "jest/globals": true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:cypress/recommended",
+    "plugin:jest/recommended",
+    "plugin:testing-library/react",
+    "plugin:json/recommended",
+    "plugin:prettier/recommended",
+    "prettier/react",
+    "prettier/@typescript-eslint",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    "@typescript-eslint",
+    "@typescript-eslint/tslint",
+    "react",
+    "cypress",
+    "jest",
+    "testing-library",
+    "prettier",
+    "import",
+    "json",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  overrides: [
+    {
+      files: ["*.js", "*.jsx"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+      },
+    },
+    {
+      files: ["**/cypress/**/*"],
+      rules: {
+        "jest/expect-expect": "off",
+        "jest/valid-expect-in-promise": "off",
+        "jest/valid-expect": "off",
+        "jest/no-standalone-expect": "off",
+        "testing-library/await-async-query": "off",
+      },
+    },
+    {
+      files: [
+        "**/__tests__/pages/**/*.test.tsx",
+        "**/__tests__/pages/**/*.test.jsx",
+        "**/components/**/*.test.tsx",
+        "**/components/**/*.test.jsx",
+      ],
+      rules: {
+        // off because of testing library
+        "jest/valid-expect": "off",
+        "jest/expect-expect": "off",
+      },
+    },
+    {
+      files: [
+        "babel.config.js",
+        "jest.config.js",
+        "release.config.js",
+        ".eslintrc.js",
+        "*.json",
+      ],
+      parser: "esprima",
+    },
+  ],
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        varsIgnorePattern: "^_",
+        argsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
+    "react/prop-types": "off",
+    "react/display-name": "off",
+    "jest/no-mocks-import": "off",
+    "prettier/prettier": ["error", { semi: false }],
+    "prefer-spread": "off",
+    "no-console": "error",
+    "no-irregular-whitespace": [
+      "error",
+      {
+        skipStrings: true,
+        skipTemplates: true,
+      },
+    ],
+    "no-restricted-imports": [
+      "error",
+      {
+        paths: [
+          {
+            name: "next/link",
+            message: 'Please import from "~/components/appLink.tsx" instead.',
+          },
+          {
+            name: "next/router",
+            importNames: ["default"],
+            message: 'Please import from "~/server/routing/routes.ts" instead.',
+          },
+        ],
+      },
+    ],
+    // TODO: we had that enabled but also disabled on in multiple files
+    "sort-keys": "off",
+    "import/no-internal-modules": [
+      "error",
+      {
+        allow: [
+          "**/configuration/**/*",
+          "**/configuration/*",
+          "@testing-library/**/*",
+          "next/*",
+          "next-i18next/*",
+          "react-dom/*",
+        ],
+      },
+    ],
+    "json/*": ["error", "allowComments"],
+    "react-hooks/exhaustive-deps": "off",
+  },
+}
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-// tslint:disable-next-line:no-console
-console.log("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
