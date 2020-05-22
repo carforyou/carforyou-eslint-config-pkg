c09673a497f8682aa3a15afb04a5354c9423b346
Krzysztof Sakwerda <ksakwerda@gmail.com>
2020-05-22 15:03:51 +0200

    fix: set ecma to 2020

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "tsconfig.json",
+    parserOptions: {
+      ecmaVersion: 2020,
+    },
     sourceType: "module",
     ecmaFeatures: {
       jsx: true,
