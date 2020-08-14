ca6c3c69b0ec9eb34d32e645d472bff9a56434df
Lorenzo Kappeler <me@lnk.codes>
2020-08-14 12:19:26 +0200

    chore: note the bug and adapt the test suite

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-${PWD}/test/testRunner.sh -c .eslintrc.js -g 0 -b 17
-${PWD}/test/testRunner.sh -c .eslintrc.react.js -g 0 -b 19
-${PWD}/test/testRunner.sh -c .eslintrc.node.js -g 0 -b 19
+${PWD}/test/testRunner.sh -c .eslintrc.js -g 0 -b 16
+${PWD}/test/testRunner.sh -c .eslintrc.react.js -g 0 -b 18
+${PWD}/test/testRunner.sh -c .eslintrc.node.js -g 0 -b 18
