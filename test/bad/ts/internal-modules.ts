09b1ce6f1f750860d6bbf9bcd15a16e62810a334
Lorenzo Kappeler <me@lnk.codes>
2020-08-14 11:17:08 +0200

    chore: try to lint the type import

--- test/bad/ts/internal-modules.ts
+++ test/bad/ts/internal-modules.ts
@@ -1,2 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import picka from "@pika/pack/dist-src/index"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Entitlements } from "@carforyou/api-client/dist-types/types/models"
