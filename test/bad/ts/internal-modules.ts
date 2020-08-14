99f1f9e568137068ad839e729564513d5db1aa97
Lorenzo Kappeler <lorenzo@carforyou.ch>
2020-08-14 13:24:50 +0200

    chore: update test suite (#9)
    * test: add test cases for deep require
    
    * -: add default test command
    
    * -: lint if internal modules are imported
    
    * -: test all variations of the linter
    
    * chore: try to lint the type import
    
    * chore: note the bug and adapt the test suite

---
+++ test/bad/ts/internal-modules.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import picka from "@pika/pack/dist-src/index"
+// There is probably a bug with type imports:
+// https://github.com/benmosher/eslint-plugin-import/issues/1880
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Entitlements } from "@carforyou/api-client/dist-types/types/models"
