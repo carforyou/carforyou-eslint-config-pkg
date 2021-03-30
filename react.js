c94a94829be8cb832d12427071ca1cf1742d8102
talamcol <71456764+talamcol@users.noreply.github.com>
2021-03-30 11:05:16 +0200

    fix: unit test

--- react.js
+++ react.js
@@ -11,7 +11,7 @@ module.exports = {
     "plugin:react-hooks/recommended",
     "plugin:testing-library/react",
     "plugin:cypress/recommended",
-    "prettier/react",
+    "plugin:prettier/recommended",
   ],
   plugins: ["react", "testing-library", "cypress"],
   settings: {
