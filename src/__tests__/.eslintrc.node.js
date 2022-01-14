b835718a346ddece75f3f57df22d88d3b0b9d039
Christian Felder (masone) <ema@rh-productions.ch>
2022-01-14 17:11:54 +0100

    fix tests

--- src/__tests__/.eslintrc.node.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  extends: ["../../pkg/node.js"],
-}
--- src/__tests__/test.sh
+++ src/__tests__/test.sh
@@ -2,4 +2,4 @@
 
 ${PWD}/src/__tests__/testRunner.sh -c ${PWD}/src/__tests__/.eslintrc.js -g 0 -b 15
 ${PWD}/src/__tests__/testRunner.sh -c ${PWD}/src/__tests__/.eslintrc.react.js -g 0 -b 17
-${PWD}/src/__tests__/testRunner.sh -c ${PWD}/src/__tests__/.eslintrc.node.js -g 0 -b 17
+${PWD}/src/__tests__/testRunner.sh -c ${PWD}/src/__tests__/.eslintrc.next.js -g 0 -b 17
