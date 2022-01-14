8cea8a71f6f2706991feb6ed81f5594fc0e61291
Christian Felder (masone) <ema@rh-productions.ch>
2022-01-14 16:25:24 +0100

    move tests

--- src/__test__/.eslintrc.js
+++ src/__test__/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["./pkg/index.js"],
+  extends: ["../../pkg/index.js"],
   settings: {
     jest: {
       version: 27,
---
+++ src/__test__/.eslintrc.node.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: ["../../pkg/react.js"],
+}
